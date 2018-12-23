4796ba097bac67f008a1d4d6abf1a3faaee623c7
luo-mini <1009183116@qq.com>
2018-12-23 18:43:53 +0800

    wx小程序

---
+++ src/components/menu.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="box">
+        <ul class="list-box">
+            <li v-for="(item,i) in list" :key="i" :class="item.isT?'nav-3-li':''"><a :href="item.url">{{ item.name }}</a></li>
+        </ul>
+        <i>0110</i>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ["list"],
+    data () {
+        return {}
+    },
+}
+</script>
+
+<style scoped>
+.box{
+    z-index: 1;
+    width: 100%;
+    background: red;
+    position: fixed;
+    bottom: 0;left: 0;
+}
+.list-box{
+    width: 100%;
+    margin: 0;padding: 10rpx;
+}
+ul.list-box li{
+    width: 232.5rpx;
+}
+.nav-3-li{
+    width: 310rpx;
+}
+</style>
+
+
